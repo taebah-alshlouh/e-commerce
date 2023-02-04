45bdb50949dfaede86e668300fa9241b4d99d4ae
unknown <taebah.bassam@gmail.com>
2023-02-04 03:11:08 +0300

    products review section

--- script.js
+++ script.js
@@ -59,4 +59,33 @@ searchButton.addEventListener('click', function( ) {
 })      
 tClose.addEventListener('click',function(){
     showClass.classList.remove('showsearch')
-})  
+}) 
+
+//show dpt menu 
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+      dptClass = document.querySelector('.site');
+dptButton.addEventListener('click', function() {
+         dptClass.classList.toggle('showdpt')
+})
+
+//Product Image Slider
+var productThumb = new Swiper ('.small-image', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView:3,
+  freeMode: true,
+  watchSlidesProgrezz: true,
+  breakpoints: {
+    481:{
+      spaceBetween: 32,
+    }
+  }
+});
+var productBig = new Swiper ('.big-image', {
+  loop: true,
+  autoHeight: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
