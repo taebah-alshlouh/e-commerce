74303786b5fe32184de8c59953412e08f43c4be8
unknown <taebah.bassam@gmail.com>
2023-02-15 19:58:14 +0300

    last change on cart page

--- script.js
+++ script.js
@@ -87,6 +87,9 @@ var productBig = new Swiper ('.big-image', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper:productThumb,
   }
 })
 
@@ -95,7 +98,7 @@ var stocks = document.querySelectorAll('.products .stock');
 for (let x = 0; x < stocks.length; x++) {
   let stock = stocks[x].dataset.stock,
   available = stocks[x].querySelector('.qty-available').innerHTML,
-  sold = stocks[x].querySelector['.qty-sold'].innerHTML,
+  sold = stocks[x].querySelector('.qty-sold').innerHTML,
   percent = sold*100/stock;
 
   stocks[x].querySelector('.available').style.width = percent + '%';
