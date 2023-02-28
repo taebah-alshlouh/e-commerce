71e2d616f3454e54fe4d9926ad401d183327e1ee
unknown <taebah.bassam@gmail.com>
2023-02-28 23:22:27 +0300

    banner section in home page

--- script.js
+++ script.js
@@ -44,14 +44,21 @@ if (sorter) {
     sorter.querySelector('ul').classList.toggle('show')
   }))
 }
+//TABBED
+const trigger = document.querySelectorAll('.tabbed-trigger'),
+      content = document.querySelectorAll('.tabbed > div');
+trigger.forEach((btn) => {
+  btn.addEventListener('click', function() {
+    let dataTarget = this.dataset.id,
+    body = document.querySelector(`#${dataTarget}`);
 
+    trigger.forEach((b) => b.parentNode.classList.remove('active'));
+    trigger.forEach((s) => s.classList.remove('active'));
+    this.parentNode.classList.add('active');
+    body.classList.add('active');
 
-
-
-
-
-
-
+  })
+})
 
 //SLIDER
 
